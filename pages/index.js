4ecfa3f333acc380e5454efe1223c3b330b83e7a
Jagjot <jaysing989@gmail.com>
2021-08-29 23:44:27 +0530

    added icons

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { logoutUser } from "../utils/authUser";
 import styled from "styled-components";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Home({ user }) {
   return (
     <>
       <div onClick={() => logoutUser(user.email)}>LOGOUT USER</div>
+      <SearchIcon />
     </>
   );
 }
