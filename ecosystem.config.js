12af7c65ec913a6d7be3fb61124cc317e26ad0ab
jagjot26 <jaysing989@gmail.com>
2023-01-29 23:31:41 +0530

    add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    script: 'npm run start',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      key: 'faeshare-server-key.pem',
+      user : 'ubuntu',
+      host : '52.87.219.225',
+      ref  : 'origin/main',
+      repo : 'git@github.com:Jagjot26/faeshare.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
