e1204c3b030d8add4a97f130ba63f8f19d6fc38d
jagjot26 <jaysing989@gmail.com>
2023-01-30 03:28:26 +0530

    updated baseurl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://faeshare.herokuapp.com";
+    : "http://54.236.48.80";
 
 export default baseUrl;
